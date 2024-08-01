6a3c2826058f7de8259d65fe56c00e5eb03e9852
Maggie Bupp <mvb6309@psu.edu>
2024-08-01 11:14:29 -0400

    extractor 200

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 
-print("Hello world from python")
+import requests
+from keboola.component.base import ComponentBase
+class Component(ComponentBase):
+    def __init__(self):
+        super().__init__()
+    def run(self):       
+        params = self.configuration.parameters
+        Token = params['BearerToken']
+        dataset=params['Dataset']
+        url = "https://api.roe-ai.com/v1/datasets/files/{id}/download/".format(id=dataset)
+        headers = {"Authorization": "Bearer {Token}".format(Token=Token)}
+
+        response = requests.request("GET", url, headers=headers)
+
+        print(response)
+
+if __name__ == "__main__":
+        comp = Component()
+        # this triggers the run method by default and is controlled by the configuration.action parameter
+        comp.execute_action()
+    
