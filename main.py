ee7de4489baff7cf869ce8249010f979adb75f90
Maggie Bupp <mvb6309@psu.edu>
2024-08-13 12:34:54 -0400

    still working

--- main.py
+++ main.py
@@ -12,11 +12,18 @@ class Component(ComponentBase):
         FileName=params['FileName']
         fileID=params['File_ID']
         OutFile=("out/files/{filename}").format(filename=FileName)
-        url = "https://api.roe-ai.com/v1/datasets/files/{id}/download/".format(id=fileID)
-        headers = {"Authorization": "Bearer {Token}".format(Token=Token)}
-        
-        response = requests.request("GET", url, headers=headers)
+        # url = "https://api.roe-ai.com/v1/datasets/files/{id}/download/".format(id=fileID)
+        # url = "https://api.roe-ai.com/v1/datasets/files/".format(id=fileID)
+        url = "https://api.roe-ai.com/v1/database/query/"
 
+        payload = {"query": "SELECT file FROM dataset_default"}
+        headers = {"Authorization": "Bearer {Token}".format(Token=Token)}
+
+        response = requests.request("POST", url, json=payload, headers=headers)
+       
+       
+        # response = requests.request("GET", url, headers=headers)
+        JSON_response=response.json()
         # Save the content into out/files
         if response.status_code == 200:
             directory = os.path.dirname(OutFile)
@@ -24,13 +31,25 @@ class Component(ComponentBase):
                 os.makedirs(directory)
 
             with open(OutFile, "wb") as fp:
-                fp.write(response.content)
+                rows=(JSON_response[0]["result_rows"])
+                for row in rows:
+                    #API call
+                    fileid=row[0].split("_")
+                    fileid=fileid[1]
+                    
+                    url = "https://api.roe-ai.com/v1/datasets/files/{id}/download/".format(id=fileid)
+                    headers = {"Authorization": "Bearer {Token}".format(Token=Token)}
+                    response = requests.request("GET", url, headers=headers)
+                
+                    # fp.write("{row}".format(row=row))
+                    print(response.content)
+                    fp.write(response.content)
             print("Image downloaded successfully.")
         else:
             print(f"Failed to download the image. Status code: {response.status_code}")
        
+        #api call for each file
         
-
         
 if __name__ == "__main__":
         comp = Component()
