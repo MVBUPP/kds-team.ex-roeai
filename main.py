3177ae92b1e91246922f24881425bff6d38445e4
Maggie Bupp <mvb6309@psu.edu>
2024-08-15 12:08:38 -0400

    fixing mistakes

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 
 import requests
 import os
-from keboola.component.base import ComponentBase
+from keboola.component.base import ComponentBase, sync_action
 import csv
+from keboola.component.sync_actions import ValidationResult, MessageType, SelectElement
+from typing import List
 class Component(ComponentBase):
     def __init__(self):
         super().__init__()
 
-    def showTables(self):
+    @sync_action('tables')
+    def showTable(self)-> List[SelectElement]:
         params = self.configuration.parameters
         Token = params['BearerToken']
         FileName=params['FileName']
@@ -27,7 +30,9 @@ class Component(ComponentBase):
         # response = requests.request("GET", url, headers=headers)
         JSON_response=response.json()
         rows=(JSON_response[0]["result_rows"])
-        return rows
+        return [SelectElement(value=rows[1], label="Value 1 label"),
+                SelectElement(value=rows[2], label="Value 2 label")]
+       
        
        
     def run(self):       
@@ -35,27 +40,52 @@ class Component(ComponentBase):
         Token = params['BearerToken']
         FileName=params['FileName']
         fileID=params['File_ID']
-        OutFile=("out/files/{filename}").format(filename=FileName)
-        url = "https://api.roe-ai.com/v1/datasets/files/{id}/download/".format(id=fileID)
-        headers = {"Authorization": "Bearer {Token}".format(Token=Token)}
-        
-        response = requests.request("GET", url, headers=headers)
+        OutFile=("out/tables/{filename}").format(filename=FileName)
+        # url = "https://api.roe-ai.com/v1/datasets/files/{id}/download/".format(id=fileID)
+        # url = "https://api.roe-ai.com/v1/datasets/files/".format(id=fileID)
+        url = "https://api.roe-ai.com/v1/database/query/"
 
+        payload = {"query": "SELECT name, file FROM dataset_examples"}
+        # payload = {"query": "SHOW TABLES"}
+        # payload = {"query": "Create Table test1()"}
+        headers = {"Authorization": "Bearer {Token}".format(Token=Token)}
+
+        response = requests.request("POST", url, json=payload, headers=headers)
+       
+        print(response.content)
+
+        # response = requests.request("GET", url, headers=headers)
+        JSON_response=response.json()
         # Save the content into out/files
         if response.status_code == 200:
             directory = os.path.dirname(OutFile)
             if not os.path.exists(directory):
                 os.makedirs(directory)
 
-            with open(OutFile, "wb") as fp:
-                fp.write(response.content)
-            print("Image downloaded successfully.")
+            with open(OutFile, "w") as fp:
+                csvwriter=csv.writer(fp)
+                csvwriter.writerow(['FileName','FileID'])
+                rows=(JSON_response[0]["result_rows"])
+                
+                for row in rows:
+                    #API call
+                    fileid=row[1].split("_")
+                    fileid=fileid[1]
+                    writerow=[row[0],fileid]
+                    # url = "https://api.roe-ai.com/v1/datasets/files/{id}/download/".format(id=fileid)
+                    # headers = {"Authorization": "Bearer {Token}".format(Token=Token)}
+                    # response = requests.request("GET", url, headers=headers)
+                
+                    # fp.write("{row}".format(row=row))
+                    # print(response.content)
+                    # fp.write(response.content)
+                    csvwriter.writerow(writerow)
+            print("Image(s) downloaded successfully.")
         else:
             print(f"Failed to download the image. Status code: {response.status_code}")
        
-        
-
-        
+        #api call for each file
+            
 if __name__ == "__main__":
         comp = Component()
         # this triggers the run method by default and is controlled by the configuration.action parameter
